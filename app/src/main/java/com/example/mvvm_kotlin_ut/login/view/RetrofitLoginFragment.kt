60e1eff60da2532ad78ca81b4eeef63731608c13
Ravi <ravisaini1990@gmail.com>
2020-07-08 12:27:48 +0530

    Remove unused commented line

--- app/src/main/java/com/example/mvvm_kotlin_ut/login/view/RetrofitLoginFragment.kt
+++ app/src/main/java/com/example/mvvm_kotlin_ut/login/view/RetrofitLoginFragment.kt
@@ -17,7 +17,6 @@ import com.example.mvvm_kotlin_ut.login.viewmodel.LoginViewModelFactory
 import com.example.mvvm_kotlin_ut.openActivity
 import kotlinx.android.synthetic.main.fragment_retrofit_login.*
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
